909a1c9f359372804580a248ee810bc04a1bb3a6
DhrupalKagathara <131572288+DhrupalKagathara@users.noreply.github.com>
2024-11-08 18:17:27 +0530

    color

--- e-signature/src/Components/SignaturePad.jsx
+++ e-signature/src/Components/SignaturePad.jsx
@@ -10,11 +10,11 @@ const SignaturePad = () => {
 
   const saveSignature = () => {
     if (!sigCanvas.current.isEmpty()) {
-      const signatureDataUrl = sigCanvas.current.toDataURL("image/png");
+      const signatureDataUrl = sigCanvas.current.toDataURL("image/jpeg");
       
       const downloadLink = document.createElement("a");
       downloadLink.href = signatureDataUrl;
-      downloadLink.download = "signature.png";
+      downloadLink.download = "signature.jpeg";
       downloadLink.click();
     } else {
       console.log("Signature pad is empty.");
@@ -24,10 +24,11 @@ const SignaturePad = () => {
   return (
     <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md w-full max-w-md mx-auto mt-10">
       <h2 className="text-2xl font-semibold text-gray-700 mb-4">Signature Pad</h2>
-      <div className="border-2 border-black bg-gray-300 rounded-md mb-4 w-full h-64">
+      <div className="border-2 border-gray-300 bg-white rounded-md mb-4 w-full h-64">
         <SignatureCanvas
           ref={sigCanvas}
-          penColor="white"
+          backgroundColor="white"
+          penColor="black"
           canvasProps={{
             className: "w-full h-full rounded-md",
           }}
@@ -38,13 +39,13 @@ const SignaturePad = () => {
           onClick={clearSignature}
           className="px-4 py-2 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600"
         >
-          Clear
+          
         </button>
         <button
           onClick={saveSignature}
           className="px-4 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600"
         >
-          Save
+          Download
         </button>
       </div>
     </div>
